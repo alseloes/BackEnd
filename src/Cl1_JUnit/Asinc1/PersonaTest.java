51de95068fd9e1ad6c2d656689d3a3bfe9f1c88c
Juan Carlos Alvarez <alseloes@gmail.com>
2023-07-31 14:52:55 -0500

    Update PersonaTest.java

--- src/Cl1_JUnit/Asinc1/PersonaTest.java
+++ src/Cl1_JUnit/Asinc1/PersonaTest.java
@@ -15,23 +15,21 @@ public class PersonaTest {
         //CUANDO
 
         //ENTONCES
-        Assertions.assertEquals("Alvarez, Juan Carlos", juan.mostrarNombreCompleto() );
-        Assertions.assertEquals("Alvarez, Matias", matias.mostrarNombreCompleto());
+        Assertions.assertEquals("Alvarez, Juan Carlos", juan.getNombreCompleto() );//Mediante getter
+        Assertions.assertEquals("Alvarez, Matias", matias.mostrarNombreCompleto());//Mediante método
     }
 
     @Test
     public void testEsMayorDeEdad () {
         //DADO
         LocalDate fechaHoy = LocalDate.of(2021, 9, 21);
+        Persona juan = new Persona("Juan Carlos", "Alvarez", "alseloes@gmail.com", 41);
+        Persona matias = new Persona("Matias","Alvarez", "email@gmail.com", 14);
         //CUANDO
 
         //ENTONCES
-        Persona persona1 = new Persona("Pedro", "Gomez", "pedro@example.com", 6);
-        Assertions.assertFalse(persona1.esMayorDeEdad(fechaHoy));
-
-        Persona persona2 = new Persona("Ana", "Lopez", "ana@example.com", 19);
-        Assertions.assertFalse(persona2.esMayorDeEdad(fechaHoy));
-
+        Assertions.assertFalse(juan.esMayorDeEdad(fechaHoy));
+        Assertions.assertFalse(matias.esMayorDeEdad(fechaHoy));
 
 
     }
